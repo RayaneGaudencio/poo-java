1129867bf9b19eb0be4fac20cfae9c775e024042
Rayane Gaudencio <rayanegaudencio11@gmail.com>
2022-10-25 12:41:58 -0300

    adicionando editora e genero a classe livro

--- Associacao/src/br/com/unit/associacao/Livro.java
+++ Associacao/src/br/com/unit/associacao/Livro.java
@@ -5,12 +5,16 @@ public class Livro {
 	private String titulo;
 	private String ISBN;
 	private int numeroPaginas;
+	private Editora editora;
+	private Genero genero;
 	
-	public Livro(int idLivro, String titulo, String iSBN, int numeroPaginas) {
+	public Livro(int idLivro, String titulo, String ISBN, int numeroPaginas, Editora editora, Genero genero) {
 		this.idLivro = idLivro;
 		this.titulo = titulo;
-		ISBN = iSBN;
+		this.ISBN = ISBN;
 		this.numeroPaginas = numeroPaginas;
+		this.editora = editora;
+		this.genero = genero;
 	}
 
 	public String getTitulo() {
@@ -25,8 +29,8 @@ public class Livro {
 		return ISBN;
 	}
 
-	public void setISBN(String iSBN) {
-		ISBN = iSBN;
+	public void setISBN(String ISBN) {
+		this.ISBN = ISBN;
 	}
 
 	public int getNumeroPaginas() {
@@ -39,6 +43,22 @@ public class Livro {
 
 	public int getIdLivro() {
 		return idLivro;
+	}
+
+	public Editora getEditora() {
+		return editora;
+	}
+
+	public void setEditora(Editora editora) {
+		this.editora = editora;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
 	}
 	
 }
