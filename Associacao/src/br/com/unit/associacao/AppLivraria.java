632eaf9517ef37eb7fbdf20f900bbf2bcef0b51f
Rayane Gaudencio <rayanegaudencio11@gmail.com>
2022-10-25 13:27:14 -0300

    adicionando ArrayList as classes

---
+++ Associacao/src/br/com/unit/associacao/AppLivraria.java
@@ -0,0 +1,13 @@
+package br.com.unit.associacao;
+import java.util.ArrayList;
+
+public class AppLivraria {
+
+	public static void main(String[] args) {
+		ArrayList<Editora> listaEditoras = new ArrayList<Editora>();
+		ArrayList<Livro> listaLivros = new ArrayList<Livro>();
+		ArrayList<Genero> listaGeneros = new ArrayList<Genero>();
+		
+	}
+
+}
